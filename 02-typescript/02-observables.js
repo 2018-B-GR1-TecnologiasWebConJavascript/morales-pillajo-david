9f616a8d7c47eb842882e178416fe5e1810b4fb0
David-Morales <david.morales01@epn.edu.ec>
2018-11-16 17:48:51 -0500

    commit

--- 02-typescript/02-observables.js
+++ 02-typescript/02-observables.js
@@ -1,7 +1,27 @@
 const rxjs = require('rxjs');
-const numeros$ = rxjs.of(1, 2, 3, 4, 5, 6);
+const map = require('rxjs/operators').map;
+const distinct = require('rxjs/operators').distinct;
+const concat = require('rxjs/operators').concat;
+const numeros$ = rxjs.of(1, "David", true, { nombre: 'david' }, [1, 2, 3, 4], new Date(), 1, 1, 1);
 console.log(numeros$);
+const promesita = (correcto) => {
+    return new Promise((resolve, reject) => {
+        if (correcto) {
+            resolve(':)');
+        }
+        else {
+            reject(':(');
+        }
+    });
+};
+const promesita$ = rxjs.from(promesita(true));
+const promesitaNoOk$ = rxjs.from(promesita(false));
 numeros$
+    .pipe(concat(promesitaNoOk$), concat(promesita$), distinct(), map((valorActual) => {
+    return {
+        data: valorActual
+    };
+}))
     .subscribe((ok) => {
     console.log('En ok', ok);
 }, (error) => {

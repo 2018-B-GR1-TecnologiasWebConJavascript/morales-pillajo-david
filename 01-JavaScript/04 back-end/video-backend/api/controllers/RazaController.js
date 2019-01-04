e5a77bdc56ce7bc5f176c357f0224a78ad6a49ee
David-Morales <david.morales01@epn.edu.ec>
2019-01-04 16:06:34 -0500

    commit

---
+++ 01-JavaScript/04 back-end/video-backend/api/controllers/RazaController.js
@@ -0,0 +1,16 @@
+/**
+ * RazaController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+  holaMundo:(req,res)=>{
+
+    return res.send('ok')
+  }
+
+};
+
--- 01-JavaScript/04 back-end/video-backend/config/datastores.js
+++ 01-JavaScript/04 back-end/video-backend/config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    url: 'mysql://david:morales@host:port/js',
 
   },
 
